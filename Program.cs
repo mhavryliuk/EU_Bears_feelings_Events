c7518e33f8dab697a8e3776ef787d246485904c7
Maksym Havryliuk <maks.havryliuk@gmail.com>
2018-05-29 10:53:57 +0300

    Made the refactoring of the application.

--- Program.cs
+++ Program.cs
@@ -4,48 +4,34 @@ namespace _20180320_Task1_Events
 {
     public delegate void EventDelegate();
 
-    class Subject
+    internal class Program
     {
-        // 2. Declaring Flirt and Run Events.
-        public event EventDelegate BeginningOfFlirting = null;
-
-        // 5. A method that signals the start of an event.
-        public void FlirtEvent()
-        {
-            if (BeginningOfFlirting != null)
-            {
-                Console.WriteLine("Rose publicly announced her choice...\n");
-                BeginningOfFlirting.Invoke();
-            }
-        }
-    }
-
-    class Program
-    {
-        // 6. Methods of event handlers.
-        static private void Flirt_Handler()
+        /// <summary>
+        /// First method of event handler.
+        /// </summary>
+        private static void Flirt_Handler()
         {
             Console.WriteLine("Jack now meets Rose.");
         }
 
-        static private void Run_Handler()
+        /// <summary>
+        /// Second method of event handler.
+        /// </summary>
+        private static void Run_Handler()
         {
             Console.WriteLine("Billy was upset and ran away...");
         }
 
-        static void Main()
+        private static void Main()
         {
-            // 1. Create a publisher.
-            Subject Rose = new Subject();
+            
+            Subject Rose = new Subject();   // Create a publisher.
 
-            // 3. Attaching event handlers.
             Rose.BeginningOfFlirting += Flirt_Handler;
             Rose.BeginningOfFlirting += Run_Handler;
 
-            // 4. Run the event handler method.
-            Rose.FlirtEvent();
+            Rose.FlirtEvent();              // Run the event handler method.
 
-            // 7. Rose doesn't communicate with Billy anymore.
             Console.WriteLine("\nRose doesn't communicate with Billy anymore.");
             Rose.BeginningOfFlirting -= Run_Handler;
 
---
+++ Subject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _20180320_Task1_Events
+{
+    internal class Subject
+    {
+        /// <summary>
+        /// Declaring Flirt and Run Events.
+        /// </summary>
+        public event EventDelegate BeginningOfFlirting = null;
+
+        /// <summary>
+        /// A method that signals the start of an event.
+        /// </summary>
+        public void FlirtEvent()
+        {
+            if (BeginningOfFlirting != null)
+            {
+                Console.WriteLine("Rose publicly announced her choice...\n");
+                BeginningOfFlirting.Invoke();
+            }
+        }
+    }
+}
